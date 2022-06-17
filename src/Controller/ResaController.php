e59abc634681543a9d6626e754b93d79e457025c
davydebut <spanudavy@gmail.com>
2022-06-17 11:46:00 +0200

    probleme pas resolu resa

--- src/Controller/ResaController.php
+++ src/Controller/ResaController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Resa;
+use App\Form\ResaType;
 use App\Repository\ResaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -9,12 +11,22 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ResaController extends AbstractController
 {
-    #[Route('/resa', name: 'app_resa')]
+    /* #[Route('/resa', name: 'app_resa')]
     public function index(ResaRepository $resaRepository): Response
     {
         $resa = $resaRepository->findAll();
         return $this->render('resa/index.html.twig', [
             'resa' => $resa,
+        ]);
+    }    */
+
+    #[Route('/resa', name: 'resa_edit')]
+    public function edit(Resa $resa): Response
+    {
+        $form = $this->createForm(ResaType::class, $resa);
+        dump($form);
+        return $this->render('resa/index.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
